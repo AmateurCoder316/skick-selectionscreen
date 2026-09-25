c36945d3a4f471ed0c9065e4e25dd6423ccc03be
AmateurCoder316 <148056701+AmateurCoder316@users.noreply.github.com>
2026-09-25 18:40:33 +0300

    Build selection screen v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './styles/App.css'
 
+import businessBackground from './assets/yrityksille-background.png'
+import consumerBackground from './assets/yksityishenkilöille-background.png'
+
 function App() {
   return (
-    <>
+    <main className="selection-screen">
+      <section
+        className="selection-side"
+        style={{ backgroundImage: `url(${businessBackground})` }}
+      >
+        <h1>YRITYKSILLE</h1>
+      </section>
 
-    </>
+      <section
+        className="selection-side"
+        style={{ backgroundImage: `url(${consumerBackground})` }}
+      >
+        <h1>YKSITYISHENKILÖILLE</h1>
+      </section>
+    </main>
   )
 }
 
